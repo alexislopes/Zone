2654a81900121a663fed3d482defef8021acb02a
alexislopes <alexisdrops@gmail.com>
2018-11-16 19:17:28 -0200

    Esqueleto do projeto

--- app/src/main/java/zone/com/br/src/view/Login.java
+++ app/src/main/java/zone/com/br/src/view/Login.java
@@ -2,14 +2,27 @@ package zone.com.br.src.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 import zone.com.br.src.R;
 
 public class Login extends AppCompatActivity {
 
+    EditText edtUsuario, edtSenha;
+    Button btnEntrar, btnCadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principal);
+        setContentView(R.layout.activity_login);
+        conectaJavaXML();
+    }
+
+    public void conectaJavaXML(){
+        edtUsuario = findViewById(R.id.edtUsuario);
+        edtSenha = findViewById(R.id.edtSenha);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
     }
 }
